08eee1b29176148ba599cf31f4c180d88167ba18
Ivajkin Timofej <timatnet@gmail.com>
2012-04-12 00:37:03 +1100

    some fixes

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ $$('#top .live').addEvent('click', function() {
 		"margin-left": 0
 	});
 });
+
+/*$$('.sphere').addEvent('click', function() {
+	var s = [$$('.sphere .s1'), $$('.sphere .s2'), $$('.sphere .s3')];
+	s.forEach(function(s) {
+		if(!s.__initialised__) {
+			s.__initialised__ = true;
+			s.velocity = [Math.random(), Math.random()];
+		}
+		var dtime = 1000/30;
+		setInterval(function(s) {
+			s.velocity[1] += 0.1*dtime;
+			s.morph({top: s.velocity[1]});
+		}, dtime, s);
+	});
+});*/
