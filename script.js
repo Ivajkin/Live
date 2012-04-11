404492f0a53a33c1a7e8b56f6588d0c162dab30b
Ivajkin Timofej <timatnet@gmail.com>
2012-04-12 00:52:03 +1100

    Tried to add spheres animation

--- script.js
+++ script.js
@@ -8,18 +8,19 @@ $$('#top .live').addEvent('click', function() {
 		"margin-left": 0
 	});
 });
-
-/*$$('.sphere').addEvent('click', function() {
-	var s = [$$('.sphere .s1'), $$('.sphere .s2'), $$('.sphere .s3')];
-	s.forEach(function(s) {
+/*
+$$('.sphere').addEvent('click', function() {
+	var sa = [$$('.sphere .s1'), $$('.sphere .s2'), $$('.sphere .s3')];
+	for(var i = 0; i < sa.length; ++i) {
+		var s = sa[i];
 		if(!s.__initialised__) {
 			s.__initialised__ = true;
 			s.velocity = [Math.random(), Math.random()];
 		}
-		var dtime = 1000/30;
+		var dtime = 1000/1;
 		setInterval(function(s) {
 			s.velocity[1] += 0.1*dtime;
-			s.morph({top: s.velocity[1]});
+			s.morph({"top": s.velocity[1]});
 		}, dtime, s);
-	});
+	}
 });*/
